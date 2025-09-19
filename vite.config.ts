4479515cdc4ba779826bc3707bb16c1606a081c4
Franckkaezer <franckkaezer30@gmail.com>
2025-09-19 15:43:50 +0000

    Fix: Mise a jour header

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
+
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      // ✅ Force Vite à utiliser la bonne entrée de vercel analytics
-      "@vercel/analytics/react": "@vercel/analytics/dist/react/index.mjs",
+  plugins: [react(),tailwindcss(),],
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,"./src")
     },
-  },
-  optimizeDeps: {
-    include: ["@vercel/analytics/react"], // ✅ prébuild côté React
   },
 })
